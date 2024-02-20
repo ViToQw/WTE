fc7f07435aff08ef7b75a7a91630074d8e82e643
ViToQw <vt05204912@gmail.com>
2024-02-20 21:26:39 +0400

    Добавьте файлы проекта.

---
+++ WTE/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WTE.Models;
+
+namespace WTE.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+/*
+        [HttpPost]
+        public IActionResult Check()
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+        }*/
+
+    }
+
+
+}
