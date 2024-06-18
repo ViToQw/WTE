fbb24279b00773691749f134979297d46930be4d
Alexander <zininsacha@gmail.com>
2024-06-18 06:40:15 +0400

    сейчас не работает анимация сердечек потому что тот список, который с рецептами не обновляется с помощью реактивного vue

--- WTE/wwwroot/js/FavoriteStar.js
+++ WTE/wwwroot/js/FavoriteStar.js
@@ -1,8 +1,4 @@
 ﻿const FavoriteStar = {
-    model: {
-        prop: 'isFavorite',
-        event: 'update:is-favorite',
-    },
     props: {
         recipeId: {
             type: Number,
@@ -19,11 +15,14 @@
     },
     template: `
     <div @click="toggleFavorite" class="favorite-star">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" :class="['heart-icon', { 'is-favorite': isFavorite }]">
-        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
-      </svg>
+      <img v-bind:src="heartIconSrc" class="heart-icon" />
     </div>
   `,
+    computed: {
+        heartIconSrc() {
+            return this.isFavorite ? '../Content/Images/icons8-heart-red.png' : '../Content/Images/icons8-heart.png';
+        },
+    },
     methods: {
         toggleFavorite() {
             const url = `http://hnt8.ru:8222/favorites/${this.userId}/recipes/${this.recipeId}`;
@@ -42,21 +41,16 @@
     style: `
     .favorite-star {
       cursor: pointer;
-      width: 24px;
-      height: 24px;
+      display: inline-block; /* Чтобы размер блока подстраивался под изображение */
     }
 
     .heart-icon {
       width: 24px;
       height: 24px;
-      transition: fill 0.3s ease; /* Анимация изменения цвета */
-    }
-
-    .heart-icon.is-favorite {
-      fill: red; /* Цвет сердца в избранном состоянии */
-      width: 24px;
-      height: 24px;
+      transition: opacity 0.3s ease; /* Анимация изменения прозрачности */
     }
   `
 };
+
+
 
